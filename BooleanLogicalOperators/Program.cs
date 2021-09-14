a3fd915b1e92d5505316b56524cebd0bee606532
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:06:04 +0300

    Add OR And

---
+++ BooleanLogicalOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BooleanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta enda nimi");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool");
+            string UserPassword = Console.ReadLine();
+
+            if (UserName == "admin" && UserPassword == "admin1234")
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Proovi uuesti");
+            }
+
+
+        }
+    }
+}
---
+++ login/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta enda nimi");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool");
+            string UserPassword = Console.ReadLine();
+
+            if (UserName != "admin" || UserPassword != "admin1234")
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Proovi uuesti");
+            }
+        }
+    }
+}
